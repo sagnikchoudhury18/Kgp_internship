ee2c15e5edef1f3a265ef51d69721a6befd14c43
SAGNIK CHOUDHURY <sagnikchoudhury18@gmail.com>
2018-05-24 18:20:18 +0530

    Add files via upload

---
+++ servo_arm_new2/servo_arm_new2.ino
@@ -0,0 +1,140 @@
+#include<Servo.h>
+
+Servo s[17];
+
+int i=0;
+
+class Servo_Motor
+{
+  public:
+  int deg,pos,dir;
+  void move_Motor(int deg,int pos,int dir,int servo_obj,int motor_num);               //number contains which servo to move
+  int update_pos(int curr_pos,int final_pos,int dir);     //curr_pos  will contain the pos of current passing object
+}m[12];
+
+void setup()
+{
+  Serial.begin(9600);
+  int l=2;                            //pins
+  m[0].pos=0,m[1].pos=0,m[2].pos=0,m[3].pos=0;
+  for(int k=2;k<14;k++)             //Servo objects
+  {
+  s[k].attach(l);
+  l++;
+  }
+
+  for(int k=2;k<14;k++)
+  s[k].write(0);
+
+  delay(200);
+
+}
+
+
+void loop()
+{
+  char check=0;
+  if(Serial.available())
+  {
+    check=Serial.read();
+    Serial.println(check);
+    }
+
+switch(check)
+{
+  case 'A':
+  m[0].move_Motor(60,m[0].pos,1,2,0);
+  break;
+  case 'B':
+  m[1].move_Motor(60,m[1].pos,1,3,1);
+  break;
+/*  case 'C':
+  m3_RA.move_Motor(60,m3_RA.pos,1);*/
+  
+  case '1':
+  m[0].move_Motor(60,m[0].pos,0,2,0);
+  break;
+  
+  }
+
+ delay(500);
+ }
+
+
+ void Servo_Motor::move_Motor(int deg,int pos,int dir,int servo_obj,int motor_num)       //servo_num contains the servo object
+
+{
+ Serial.println(dir);
+ Serial.println(pos);
+ m[motor_num].dir=dir;
+  if(m[motor_num].dir==1)                //rotate clockwise
+  {
+    Serial.println("Executing cond ");
+    Serial.print("Val of pos  ");
+    Serial.println(m[motor_num].pos);
+    if(m[motor_num].pos<179)
+      {
+        int a=update_pos(m[motor_num].pos,deg,m[motor_num].dir);
+        Serial.print("Val of a      :");
+        Serial.println(a);
+        for(i=m[motor_num].pos;   i<a;  i++){
+          s[servo_obj].write(i);
+          delay(10);
+          }
+       m[motor_num].pos=a;
+        Serial.print("Val of pos                 :");
+        Serial.println(m[motor_num].pos);
+       }
+  }
+
+    if(m[motor_num].dir==0)
+    {
+    Serial.println("Executing cond ");
+    Serial.print("Val of pos  ");
+    Serial.println(m[motor_num].pos);
+    if(m[motor_num].pos>0)
+      {
+        int a=update_pos(m[motor_num].pos,deg,dir);
+        Serial.print("Val of a      :");
+        Serial.println(a);
+        for(i=m[motor_num].pos;   i>a;  i--){
+          s[servo_obj].write(i);
+          delay(10);
+          }
+        m[motor_num].pos=a;
+        Serial.print("Val of pos                 :");
+        Serial.println(m[motor_num].pos);
+       }
+      
+      
+      }
+       
+    
+    }
+  
+  
+ 
+ 
+  int Servo_Motor::update_pos(int curr_pos,int final_pos,int d)//d for direction
+  {
+    Serial.print("Current pos  :");
+    Serial.println(curr_pos);
+    //Serial.print("final_pos");
+    Serial.print("Val of d");
+    Serial.println(d);
+
+    if(d==1)
+    {
+    final_pos=curr_pos+final_pos;
+    Serial.println(final_pos);
+    }
+    else if(d==0)
+    {
+    final_pos=curr_pos-final_pos;
+    }
+    
+    return final_pos;
+    
+    
+    }
+
